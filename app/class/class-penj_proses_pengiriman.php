f2c39c5949200ad1a3d8e746df1d52bf9ad5b175
Laurentius Kevin <laurentiuskevin44@gmail.com>
2019-05-16 11:09:43 +0700

    Revisi Proses Pengiriman. Menambah kolom status pengiriman

--- app/class/class-penj_proses_pengiriman.php
+++ app/class/class-penj_proses_pengiriman.php
@@ -8,16 +8,18 @@ $case = $_POST['case'];
 
 switch ($case) {
   case 'list':
-    $sql = "SELECT no_transaksi, DATE_FORMAT(tgl_transaksi,'%d %M %Y, Pk %H:%i:%s') AS tgl_transaksi, 
+    $sql = "SELECT kasir_mst.no_transaksi, DATE_FORMAT(tgl_transaksi,'%d %M %Y, Pk %H:%i:%s') AS tgl_transaksi, 
             DATE_FORMAT(tgl_kirim,'%d %M %Y, Pk %H:%i:%s') AS tgl_kirim,
             cust.nama AS customer, cust.alamat, cust.kecamatan, cust.kota, cust.provinsi,
             reseller.nama AS reseller,sub.nama AS subreseller,ekspedisi.nama AS ekspedisi,
-            FORMAT(grand_total,2) AS grand_total, no_resi
+            FORMAT(grand_total,2) AS grand_total, no_resi,
+            poh.status_pengiriman AS 'status_pengiriman'
             FROM kasir_mst
             JOIN ms_customer AS cust ON kasir_mst.id_customer=cust.id_customer
             JOIN ms_reseller AS reseller ON kasir_mst.id_reseller=reseller.id_reseller
             JOIN ms_reseller AS sub ON kasir_mst.id_subreseller=sub.id_reseller
             JOIN ms_ekspedisi AS ekspedisi ON kasir_mst.id_ekspedisi=ekspedisi.id_ekspedisi
+            JOIN pengiriman_onhold AS poh ON kasir_mst.no_transaksi=poh.no_transaksi
             WHERE status_kirim IN ('1','2')";
     $query = mysqli_query($conn,$sql);
     $total = mysqli_num_rows($query);
